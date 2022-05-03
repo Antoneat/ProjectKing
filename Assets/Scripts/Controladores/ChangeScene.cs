23117844fd5bccf27cfccf552d281277a84b05c6
FrancisPrz <denix2208@gmail.com>
2022-05-03 02:53:32 -0500

    Menú Principal Funcional
    Menú principal funcional

---
+++ Assets/Scripts/Controladores/ChangeScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneId;
+    public void ChangeSceneOption()
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Saliendo del infierno");
+    }
+}

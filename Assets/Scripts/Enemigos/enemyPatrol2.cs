51070257655053d3b2aadc41620dc36d8a47e09a
Antoneat <antonellatavarone1996@gmail.com>
2022-05-28 18:57:14 -0500

    enemigo2 correciones
    corregido el manejo de ataques

---
+++ Assets/Scripts/Enemigos/enemyPatrol2.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPatrol2 : MonoBehaviour
+{
+
+	public float Speed;
+	//public float damping = 6.0f;
+
+	public UnityEngine.AI.NavMeshAgent agent;
+
+	public int destPoint = 0;
+	public Transform goal;
+
+	public float playerDistance;
+	public float awareAI;
+	public float atkRange;
+	public Enemy2 ee2;
+
+	void Start()
+	{
+		UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+
+		agent.autoBraking = false;
+
+	}
+
+	void Update()
+	{
+		playerDistance = Vector3.Distance(transform.position, goal.position);
+
+		if (playerDistance <= awareAI)
+		{
+			LookAtPlayer();
+			Debug.Log("Seen");
+			Chase();
+		}
+		else if (playerDistance > awareAI)
+		{
+			LookAtPlayer();
+		}
+
+
+		if (playerDistance <= atkRange)
+		{
+			ee2.ChooseAtk();
+		}
+		else if (playerDistance > atkRange)
+		{
+			LookAtPlayer();
+		}
+	}
+
+	void LookAtPlayer()
+	{
+		transform.LookAt(goal);
+	}
+
+
+
+	public void Chase()
+	{
+
+		transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+		//agent.SetDestination(goal.transform.position);
+		agent.destination = goal.position;
+	}
+
+
+}
+
